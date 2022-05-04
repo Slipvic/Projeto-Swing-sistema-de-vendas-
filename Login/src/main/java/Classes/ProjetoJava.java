15644b8b654703791276978d476a87ebecc189fe
Slipvic <92764645+Slipvic@users.noreply.github.com>
2022-05-04 15:42:27 -0300

    Add files via upload

---
+++ Login/src/main/java/Classes/ProjetoJava.java
@@ -0,0 +1,21 @@
+
+package Classes;
+
+import Formulários.Login;
+
+/**
+ *
+ * @author Pichau
+ */
+public class ProjetoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Login Login = new Login();
+       Login.setLocationRelativeTo(null);
+       Login.setVisible(true);    
+    }
+    
+}
---
+++ Login/src/main/java/Classes/Vendas.java
@@ -0,0 +1,24 @@
+package Classes;
+
+public class Vendas {
+
+    String formaDePagamento;
+    String dataVenda;
+
+    public String getFormaDePagamento() {
+        return formaDePagamento;
+    }
+
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    public String getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(String dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+}
