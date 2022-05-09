b8ad2b382fcf48b7ca849e7b55a5828508c8d92f
Ricardo Magalhães <ricardomrfin@gmail.com>
2022-05-09 16:47:13 -0300

    classes Cliente, Vendedor criadas

--- Login/src/main/java/Classes/Vendas.java
+++ Login/src/main/java/Classes/Vendas.java
@@ -5,20 +5,25 @@ public class Vendas {
     String formaDePagamento;
     String dataVenda;
 
-    public String getFormaDePagamento() {
-        return formaDePagamento;
-    }
-
-    public void setFormaDePagamento(String formaDePagamento) {
-        this.formaDePagamento = formaDePagamento;
-    }
-
-    public String getDataVenda() {
-        return dataVenda;
-    }
-
-    public void setDataVenda(String dataVenda) {
-        this.dataVenda = dataVenda;
-    }
+    public Venda() {
+		super();
+	}
+	public Venda(String formaDePagamento, String dataVenda) {
+		super();
+		this.formaDePagamento = formaDePagamento;
+		this.dataVenda = dataVenda;
+	}
+	public String getFormaDePagamento() {
+		return formaDePagamento;
+	}
+	public void setFormaDePagamento(String formaDePagamento) {
+		this.formaDePagamento = formaDePagamento;
+	}
+	public String getDataVenda() {
+		return dataVenda;
+	}
+	public void setDataVenda(String dataVenda) {
+		this.dataVenda = dataVenda;
+	}
 
 }
