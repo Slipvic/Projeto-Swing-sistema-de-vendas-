f3845d18674d525bb31f9ca43b2fdba311ff24cf
Slipvic <92764645+Slipvic@users.noreply.github.com>
2022-05-19 17:12:39 -0300

    Add files via upload

---
+++ Diretorio Main/src/main/java/Classes/ProjetoJava.java
@@ -0,0 +1,21 @@
+
+package Classes;
+
+import Formulários.TelaPrincipal;
+
+/**
+ *
+ * @author Pichau
+ */
+public class ProjetoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       TelaPrincipal Login = new TelaPrincipal();
+       Login.setLocationRelativeTo(null);
+       Login.setVisible(true);    
+    }
+    
+}
